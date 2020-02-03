12510cdd280e73b0b238d02076fa15fec27afe80
LASER-Yi <liangyi0007@gmail.com>
2020-02-03 23:46:56 +0800

    update entrypoint

--- qbittorrent-ee/rootfs/usr/bin/entrypoint.sh
+++ qbittorrent-ee/rootfs/usr/bin/entrypoint.sh
@@ -2,16 +2,16 @@
 #
 # entrypoint.sh
 
-addgroup -g ${GID} qbittorrent \
-adduser -h /config -s /bin/sh -G qbittorrent -D -u ${UID} qbittorrent \
+addgroup -g ${GID} qbittorrent
+adduser -h /config -s /bin/sh -G qbittorrent -D -u ${UID} qbittorrent
 
 mkdir -p /config/.config/qBittorrent
 
-[[ ! -f /config/.config/qBittorrent/qBittorrent.conf ]] && cp /etc/qBittorrent.conf /config/.config/qBittorrent/
+[[ ! -f /config/.config/qBittorrent/qBittorrent.conf ]] && cp /etc/qBittorrent/qBittorrent.conf /config/.config/qBittorrent/
 sed -i "s|Connection\\\PortRangeMin=.*|Connection\\\PortRangeMin=${PEER_PORT}|i" /config/.config/qBittorrent/qBittorrent.conf
 sed -i "s|Downloads\\\SavePath=.*|Downloads\\\SavePath=/downloads|i" /config/.config/qBittorrent/qBittorrent.conf
 
-chown -R qbittorrent:qbittorrent /config \
+chown -R qbittorrent:qbittorrent /config
 chown -R qbittorrent:qbittorrent /downloads
 
-su qbittorrent --session-command "qbittorrent-nox --webui-port=$WEIUI_PORT"
+su qbittorrent -c "qbittorrent-nox --webui-port=${WEBUI_PORT}"
