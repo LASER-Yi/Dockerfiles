b62dfbf6a1f6553702205c398914244df8ce21bb
LASER-Yi <ly0007@yeah.net>
2020-02-15 17:37:29 +0800

    add chinadns+dnsmasq

---
+++ chinadns+dnsmasq/rootfs/usr/bin/entrypoint.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+#
+# entrypoint.sh
+
+# copy filter file if no exist
+[[ ! -f /config/chnroute.txt ]] && cp -r /backup/* /config
+
+# start dnsmasq service
+dnsmasq -a 0.0.0.0 \
+--cache-size=25000 \
+--conf-file=/dev/null \
+--conf-dir=/config/dnsmasq.d/ \
+--log-facility=/dev/stdout \
+--no-resolv \
+--server=127.0.0.1#2053 \
+--user=root
+
+# start chinadns service
+chinadns -p 2053 -c /config/chnroute.txt -v
