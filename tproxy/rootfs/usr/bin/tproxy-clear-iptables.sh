f8f516149edb402c2964d1507e04743a83ec4f28
LASER-Yi <ly0007@yeah.net>
2020-02-17 13:58:12 +0800

    rename iptables clear name

--- tproxy/rootfs/usr/bin/tproxy-clear-iptables.sh
+++ tproxy/rootfs/usr/bin/tproxy-clear-iptables.sh
@@ -2,12 +2,12 @@
 
 iptables -t nat -D OUTPUT -p tcp -j SS-TCP
 iptables -t nat -D PREROUTING -p tcp -s 192.168/16 -j SS-TCP
-iptables -t nat -F SS-TCP
-iptables -t nat -X SS-TCP
+iptables -t nat -F TPROXY-TCP
+iptables -t nat -X TPROXY-TCP
 
 iptables -t mangle -D PREROUTING -p udp -s 192.168/16 -j SS-UDP
-iptables -t mangle -F SS-UDP
-iptables -t mangle -X SS-UDP
+iptables -t mangle -F TPROXY-UDP
+iptables -t mangle -X TPROXY-UDP
 
 ip route del local 0/0 dev lo table 100
 ip rule del fwmark 0xff table 100
