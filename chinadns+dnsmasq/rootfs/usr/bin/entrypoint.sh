a2539a5c06ec34f45e8efed4357f0aa7a25cf77d
LASER-Yi <ly0007@yeah.net>
2020-02-18 12:40:55 +0800

    update chinadns

--- chinadns+dnsmasq/rootfs/usr/bin/entrypoint.sh
+++ chinadns+dnsmasq/rootfs/usr/bin/entrypoint.sh
@@ -2,6 +2,32 @@
 #
 # entrypoint.sh
 
+# create a daemon for tproxy
+
+dnsmasq_pid=0
+chinadns_pid=0
+
+trap "sigterm_handler" TERM
+
+sigterm_handler() {
+echo "exiting..."
+
+if [ $dnsmasq_pid -ne 0 ]; then
+  kill -TERM "$dnsmasq_pid"
+  wait "$dnsmasq_pid"
+fi
+
+if [ $chinadns_pid -ne 0 ]; then
+  kill -TERM "$chinadns_pid"
+  wait "$chinadns_pid"
+fi
+
+exit 143;
+}
+
+# exit when fail
+set -eu
+
 # copy filter file if no exist
 [[ ! -f /config/chnroute.txt ]] && cp -r /backup/* /config
 
@@ -12,7 +38,14 @@ dnsmasq --cache-size=25000 \
 --log-facility=/dev/stdout \
 --no-resolv \
 --server=127.0.0.1#2053 \
---user=root
+--user=root 
 
-# start chinadns service
-exec chinadns -p 2053 -c /config/chnroute.txt -v
+# start chinadns service as daemon
+chinadns -p 2053 -c /config/chnroute.txt -v &
+
+# dnsmasq will folk
+dnsmasq_pid=$(pidof dnsmasq | sed "s|^\([0-9]*\)\(.*\)|\1|")
+chinadns_pid=$(pidof chinadns)
+
+wait
+set +eu
