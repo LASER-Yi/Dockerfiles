0127d0c002dc97e7ef8bbbf1310edabf41f3ec19
LASER-Yi <ly0007@yeah.net>
2020-02-19 20:26:46 +0800

    clear chnip when done

--- tproxy/rootfs/usr/bin/tproxy-update-chnroute.sh
+++ tproxy/rootfs/usr/bin/tproxy-update-chnroute.sh
@@ -5,5 +5,8 @@
 echo "Updating CHNROUTE..." 
 
 curl -sSL 'http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest' -o /tmp/chnip
+
 cat /tmp/chnip | grep ipv4 | grep CN | awk -F\| '{printf("%s/%d\n", $4, 32-log($5)/log(2))}' > /config/chnroute4.txt
 cat /tmp/chnip | grep ipv6 | grep CN | awk -F\| '{printf("%s/%d\n", $4, $5)}' > /config/chnroute6.txt
+
+rm -rf /tmp/chnip
