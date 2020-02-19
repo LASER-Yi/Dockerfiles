4938545ab76c251469b3f4f9a73594e0d1c342a4
LASER-Yi <ly0007@yeah.net>
2020-02-19 20:24:39 +0800

    support ipv6

--- tproxy/rootfs/usr/bin/tproxy-import-chnip.sh
+++ tproxy/rootfs/usr/bin/tproxy-import-chnip.sh
@@ -6,8 +6,18 @@
 echo "Import chnip rules..."
 tempfile=$(mktemp)
 
-ipset -N chnip hash:net
-for i in `cat /config/chnroute.txt`; do echo ipset -A chnip $i >> $tempfile; done
+ipset -N chnroute hash:net
+for i in `cat /config/chnroute4.txt`; do echo ipset -A chnroute4 $i >> $tempfile; done
+chmod +x $tempfile
+
+$tempfile
+
+rm -f $tempfile
+
+tempfile=$(mktemp)
+
+ipset -N chnroute6 hash:net
+for i in `cat /config/chnroute6.txt`; do echo ipset -A chnroute6 $i >> $tempfile; done
 chmod +x $tempfile
 
 $tempfile
--- tproxy/rootfs/usr/bin/tproxy-update-chnroute.sh
+++ tproxy/rootfs/usr/bin/tproxy-update-chnroute.sh
@@ -4,4 +4,6 @@
 
 echo "Updating CHNROUTE..." 
 
-curl -sSL https://raw.githubusercontent.com/PaPerseller/chn-iplist/master/chnroute.txt -o /config/chnroute.txt
+curl -sSL 'http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest' -o /tmp/chnip
+cat /tmp/chnip | grep ipv4 | grep CN | awk -F\| '{printf("%s/%d\n", $4, 32-log($5)/log(2))}' > /config/chnroute4.txt
+cat /tmp/chnip | grep ipv6 | grep CN | awk -F\| '{printf("%s/%d\n", $4, $5)}' > /config/chnroute6.txt
