f87b67607cb995599f68c42528c982ac668b47ca
LASER-Yi <ly0007@yeah.net>
2020-02-29 22:41:05 +0800

    fix error

--- tproxy/rootfs/usr/bin/tproxy-update-chnroute.sh
+++ tproxy/rootfs/usr/bin/tproxy-update-chnroute.sh
@@ -6,7 +6,7 @@ echo "Updating CHNROUTE..."
 
 curl -sSL 'http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest' -o /tmp/chnip
 
-if [ ! -f /tmp/chnip ]] 
+if [ ! -f /tmp/chnip ]
 then
     cat /tmp/chnip | grep ipv4 | grep CN | awk -F\| '{printf("%s/%d\n", $4, 32-log($5)/log(2))}' > /config/chnroute4.txt
     cat /tmp/chnip | grep ipv6 | grep CN | awk -F\| '{printf("%s/%d\n", $4, $5)}' > /config/chnroute6.txt
