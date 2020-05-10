4d8a8cfdfedf3b7c42a644c8a63734a41a4a76a5
LASER-Yi <ly0007@yeah.net>
2020-05-10 13:26:38 +0800

    add update china list script for smartdns image

---
+++ smartdns/rootfs/usr/bin/update-china-list.sh
@@ -0,0 +1,32 @@
+# !/bin/sh
+# Update china acceleration list from dnsmasq
+
+PROXYDNS_NAME="proxydns"
+
+convertList() {
+    url=$1
+    tempFile=$(mktemp)
+    filename=$(echo $url | sed 's/.*\///')
+    curl -o $tempFile -sSL $url
+
+    if [ $? -eq 0 ]
+    then
+        echo "Updating $filename..."
+        middleFile=$(mktemp)
+
+        sed -e 's|^server=\/\(.*\)\/.*$|\1|' $tempFile | egrep -v '^#' > $middleFile
+
+        mkdir -p config/smartdns.d/
+        sed -e "s|\(.*\)|nameserver /\1/$PROXYDNS_NAME|" $middleFile > config/smartdns.d/$filename
+
+        rm -f $middleFile
+    fi
+
+    rm -f $tempfile
+}
+
+convertList "https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf"
+
+convertList "https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/google.china.conf"
+
+convertList "https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/apple.china.conf"
